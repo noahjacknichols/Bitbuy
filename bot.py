2398637df1f72b0d4bf73bc7f73866972698448f
noah nichols <noahjacknichols@gmail.com>
2019-09-11 15:02:18 -0400

    rev.2

--- bot.py
+++ bot.py
@@ -1,6 +1,23 @@
-import requests
+from coinbase.wallet.client import Client
+
+API_KEY = 'iigRJVz6bBSzl71N'
+API_SECRET = 'V2j0s6Spk8wiLi6bcdFREKeHroQBH71Q'
+ACCOUNT_ID = '572e96bd-d7d6-589f-ab76-7b113b8c72e1'
+client = Client(API_KEY, API_SECRET)
 
 
-print("test")
+accounts = client.get_accounts()
+assert isinstance(accounts.data, list)
+assert accounts[0] is accounts.data[0]
+assert len(accounts[::]) == len(accounts.data)
 
-print(requests.get('https://api.github.com'))
+
+print(client.get_buy_price(currency_pair = 'BTC-CAD'))
+print(client.get_sell_price(currency_pair = 'BTC-CAD'))
+print(client.get_spot_price(currency_pair = 'BTC-CAD'))
+
+print(client.get_time())
+print(client.get_primary_account())
+
+
+print(client.get_transactions(ACCOUNT_ID))
