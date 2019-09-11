712edc822d06ae220719ec4b25c595fee4ca4f4c
Noah Nichols <noahjacknichols@gmail.com>
2019-09-11 15:22:49 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 from coinbase.wallet.client import Client
 
-API_KEY = 'iigRJVz6bBSzl71N'
-API_SECRET = 'V2j0s6Spk8wiLi6bcdFREKeHroQBH71Q'
-ACCOUNT_ID = '572e96bd-d7d6-589f-ab76-7b113b8c72e1'
+
 client = Client(API_KEY, API_SECRET)
 
 
